81d3e07606d7051cea381a36db5e9616c43f01b0
Saifallak <Saifallak@yahoo.com>
2022-06-16 19:36:18 +0200

    rm whatsapp creds.

--- config/aqwhatsapp.php
+++ config/aqwhatsapp.php
@@ -6,11 +6,11 @@ return [
     |--------------------------------------------------------------------------
     | API TOKEN TO AUTHENTICATE WITH AQ SERVER. (YOU CAN CREATE ONE FROM PROFILE).
     */
-    'api_token' => env('AQ_WHATSAPP_API_TOKEN', "3|CMgNF8eZYdUoKQ2dcb9FtBthImeXEPOZoKEC2sV4"),
+    'api_token' => env('AQ_WHATSAPP_API_TOKEN'),
 
     /*
     |--------------------------------------------------------------------------
     | SESSION UUID (THIS IS UNIQUE FOR EACH SESSION).
     */
-    'session_uuid' => env('AQ_WHATSAPP_SESSION_UUID', "96852687-2f24-417b-afca-83d5771aabfc"),
+    'session_uuid' => env('AQ_WHATSAPP_SESSION_UUID'),
 ];
