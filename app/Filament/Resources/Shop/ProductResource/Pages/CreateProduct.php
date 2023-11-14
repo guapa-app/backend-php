d819b553a33b0b07fbb30b5f56e1572602d6b785
ADdora <abdelkaderx3@gmail.com>
2023-11-14 07:32:40 +0200

    handle create product

--- app/Filament/Resources/Shop/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/Shop/ProductResource/Pages/CreateProduct.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Shop\ProductResource\Pages;
 
+use App\Enums\ProductReview;
+use App\Enums\ProductStatus;
+use App\Enums\ProductType;
 use App\Filament\Resources\Shop\ProductResource;
 use Filament\Resources\Pages\CreateRecord;
 
@@ -11,6 +14,8 @@ class CreateProduct extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        $data['review'] = ProductReview::Approved;
+        $data['status'] = ProductStatus::Published;
         $data['vendor_id'] = auth()->user()->userVendors->first()->vendor_id;
 
         return $data;
