5954a112fd0daf2a58fdc223f09986f5ee668fd3
ADdora <abdelkaderx3@gmail.com>
2024-07-29 14:09:53 +0300

    Auth (v3)     - handle unit test     - handle test env request     - remove unnecessary files

--- routes/v3/api/reviews.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\V3\ReviewController;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('/',                                        [ReviewController::class, 'index']);
-    Route::post('/',                                       [ReviewController::class, 'create']);
-});
--- routes/v3/api/taxonomies.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\V3\TaxonomyController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/',                                [TaxonomyController::class, 'index']);
-Route::get('/{id}',                            [TaxonomyController::class, 'single']);
