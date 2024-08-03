c49855bb67781e0df6a1fb6c6673313be00d8cfd
ADdora <abdelkaderx3@gmail.com>
2024-08-04 02:16:11 +0300

    New Frontend - continue handle blades

--- resources/views/frontend/single-blog.blade.php
+++ resources/views/frontend/single-blog.blade.php
@@ -1,418 +1,146 @@
-<!DOCTYPE html>
-<html dir="rtl">
-<head>
-    <meta charset="utf-8"/>
-    <meta name="description" content="guapa app"/>
-    <meta name="author" content="guapa app"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <title>
-        GUAPA
-    </title>
-    <link rel="shortcut icon" href="assets/images/logo/icon.png" type="image/x-icon"/>
-    <link rel="stylesheet" href="assets/css/lib/animate.css"/>
-    <link rel="stylesheet" href="assets/css/lib/bootstrap.min.css"/>
-    <link rel="stylesheet" href="assets/css/lib/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="assets/css/style.css"/>
-</head>
-<body>
-<nav class="navbar">
-    <div class="container">
-        <div class="contain">
-            <div class="hamburger">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
-            </div>
+@extends('frontend.layouts.app')
 
-            <a href="index.html" class="brand-name">
-                <img src="assets/images/logo/logo.svg" loading="lazy" alt=""/>
-            </a>
+@section('title')
+    {{ ucfirst(__("blog")) }}
+@endsection
+@section('content')
+    <main>
+        <section class="sub-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-12">
+                        <div class="contain">
+                            <ul class="list">
+                                <li>
+                                    <a href="{{ route('landing') }}">
+                                        الرئيسية
+                                    </a>
+                                </li>
 
-            <div class="nav-contain">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="about.html" class="nav-link">
-                            ماذا نقدم للمستخدم
-                        </a>
-                    </li>
+                                <li>
+                                    <a href="{{ route('blogs') }}">
+                                        المدونة
+                                    </a>
+                                </li>
 
-                    <li class="nav-item">
-                        <a href="download-app.html" class="nav-link">
-                            مساعدة مزود خدمة
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="about-app.html" class="nav-link">
-                            التطبيق
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="about.html" class="nav-link">
-                            عن قوابا
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="questions.html" class="nav-link">
-                            الاسئلة الشائعة
-                        </a>
-                    </li>
-                </ul>
-
-                <div class="button-contain">
-                    <a href="register.html" class="btn-signup">
-                        تسجيل كمزود خدمة
-                    </a>
-
-                    <a href="login.html" class="custom-btn primary-btn">
-                <span>
-                  تسجيل الدخول
-                </span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
-
-<main>
-    <section class="sub-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-12">
-                    <div class="contain">
-                        <ul class="list">
-                            <li>
-                                <a href="index.html">
-                                    الرئيسية
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="blogs.html">
-                                    المدونة
-                                </a>
-                            </li>
-
-                            <li>
+                                <li>
                     <span>
                       عنوان المدونة
                     </span>
-                            </li>
-                        </ul>
+                                </li>
+                            </ul>
 
-                        <h1>
-                            عنوان المدونة
-                        </h1>
+                            <h1>
+                                عنوان المدونة
+                            </h1>
 
-                        <p>
-                            لوريم إيبسوم هو نموذج إفتراضي يتم وضعه في التصاميم التي سيتم عرضها على العميل. وبعد موافقة
-                            العميل على بداية التصميم يتم إزالة هذا النص من التصميم ويتم وضع النصوص النهائية المطلوبة
-                            للتصميم.
-                        </p>
+                            <p>
+                                لوريم إيبسوم هو نموذج إفتراضي يتم وضعه في التصاميم التي سيتم عرضها على العميل. وبعد
+                                موافقة
+                                العميل على بداية التصميم يتم إزالة هذا النص من التصميم ويتم وضع النصوص النهائية المطلوبة
+                                للتصميم.
+                            </p>
+                        </div>
                     </div>
                 </div>
+
+                <img
+                    src="{{ asset('frontend/assets/images/blogs/blog.svg') }}"
+                    class="icon"
+                    loading="lazy"
+                    alt=""/>
             </div>
 
             <img
-                src="assets/images/blogs/blog.svg"
-                class="icon"
+                src="{{ asset('frontend/assets/images/icons/sub_header.png') }}"
+                class="sub-header-img"
                 loading="lazy"
                 alt=""/>
-        </div>
+        </section>
 
-        <img
-            src="assets/images/icons/sub_header.png"
-            class="sub-header-img"
-            loading="lazy"
-            alt=""/>
-    </section>
-
-    <section class="single-blog general-section">
-        <div class="container">
-            <div class="image-contain">
-                <img src="assets/images/blogs/blog_3.png" loading="lazy" alt=""/>
-            </div>
-
-            <div class="contain">
-                <div class="flex-data">
-
-                    <div class="user-img">
-                        <img
-                            src="assets/images/blogs/user.png"
-                            loading="lazy"
-                            alt=""/>
-
-                        <span>
-                  الدكتور ياسر الكبيسي
-                </span>
-                    </div>
+        <section class="single-blog general-section">
+            <div class="container">
+                <div class="image-contain">
+                    <img src="{{ asset('frontend/assets/images/blogs/blog_3.png') }}" loading="lazy" alt=""/>
                 </div>
 
-                <h2>
-                    ما سبب وجود الرؤوس السوداء بعد عملية الأنف وطريقة علاجها
-                </h2>
+                <div class="contain">
+                    <div class="flex-data">
 
-                <div class="flex-data">
+                        <div class="user-img">
+                            <img
+                                src="{{ asset('frontend/assets/images/blogs/user.png') }}"
+                                loading="lazy"
+                                alt=""/>
+
+                            <span>
+                  الدكتور ياسر الكبيسي
+                </span>
+                        </div>
+                    </div>
+
+                    <h2>
+                        ما سبب وجود الرؤوس السوداء بعد عملية الأنف وطريقة علاجها
+                    </h2>
+
+                    <div class="flex-data">
               <span class="badge">
                 نضارة
               </span>
 
-                    <div class="date">
-                        <img src="assets/images/icons/date.svg" loading="lazy" alt=""/>
+                        <div class="date">
+                            <img src="{{ asset('frontend/assets/images/icons/date.svg') }}" loading="lazy" alt=""/>
 
-                        <span>
+                            <span>
                   تاريخ النشر
                 </span>
-                    </div>
-                </div>
-
-                <h2>
-                    عنوان اخر
-                </h2>
-
-                <p>
-                    هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث
-                    يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
-                    التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
-                    العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى
-                    يولدها التطبيق.
-                </p>
-
-                <h2>
-                    عنوان اخر
-                </h2>
-
-                <p>
-                    هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث
-                    يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
-                    التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
-                    العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى
-                    يولدها التطبيق.
-                </p>
-
-                <h2>
-                    عنوان اخر
-                </h2>
-
-                <p>
-                    هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث
-                    يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
-                    التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
-                    العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى
-                    يولدها التطبيق.
-                </p>
-            </div>
-
-        </div>
-    </section>
-</main>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 col-12 mb-4">
-                <div class="contain">
-                    <a href="#" class="brand-name">
-                        <img src="assets/images/logo/logo.svg" loading="lazy" alt=""/>
-                    </a>
-
-                    <p>
-                        سجل باستخدام عنوان بريدك الإلكتروني لتبقى على اطلاع بالخصومات والتحديثات الجديدة من جميع
-                        الحملات!
-                    </p>
-
-                    <form action="" class="form-icon">
-                        <div class="form-group">
-                            <img src="assets/images/icons/search.svg" loading="lazy" alt=""/>
-
-                            <input
-                                type="text"
-                                class="form-control"
-                                placeholder="ادخل بريدك الالكتروني"/>
-
-                            <a href="#" class="custom-btn secondary-btn">
-                    <span>
-                      سجل الان
-                    </span>
-                            </a>
                         </div>
-
-                    </form>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 mb-4">
-                <div class="flex-data">
-                    <div class="contain">
-                        <h2>
-                            روابط مهمة
-                        </h2>
-
-                        <ul class="links">
-                            <li>
-                                <a href="download-app.html">
-                      <span>
-                        تطبيق مزودي الخدمة
-                      </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="about-app.html">
-                      <span>
-                        تطبيق المستخدمين
-                      </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="about.html">
-                      <span>
-                        عن قوابا
-                      </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="blogs.html">
-                      <span>
-                        المدونة
-                      </span>
-                                </a>
-                            </li>
-                        </ul>
                     </div>
 
-                    <div class="contain">
-                        <h2>
-                            للتواصل
-                        </h2>
-
-                        <ul class="links">
-                            <li>
-                                <a href="tel:5314343889" class="block">
-                      <span>
-                        للتواصل تليفونياً
-                      </span>
-
-                                    <span>
-                        9665314343889
-                      </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="mailto:info@guapa.com.sa" class="block">
-                      <span>
-                        البريد الالكتروني
-                      </span>
-
-                                    <span>
-                        info@guapa.com.sa
-                      </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-12 mb-4">
-                <div class="contain padding-contain">
                     <h2>
-                        تابعنا
+                        عنوان اخر
                     </h2>
 
-                    <ul class="links">
-                        <li>
-                            <a href="#">
-                                <img src="assets/images/footer/youtube.svg" loading="lazy" alt=""/>
+                    <p>
+                        هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى،
+                        حيث
+                        يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
+                        التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                        العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف
+                        التى
+                        يولدها التطبيق.
+                    </p>
 
-                                <span>
-                      موقع YouTube
-                    </span>
-                            </a>
-                        </li>
+                    <h2>
+                        عنوان اخر
+                    </h2>
 
-                        <li>
-                            <a href="#">
-                                <img src="assets/images/footer/x.svg" loading="lazy" alt=""/>
+                    <p>
+                        هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى،
+                        حيث
+                        يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
+                        التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                        العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف
+                        التى
+                        يولدها التطبيق.
+                    </p>
 
-                                <span>
-                      منصة إكس
-                    </span>
-                            </a>
-                        </li>
+                    <h2>
+                        عنوان اخر
+                    </h2>
 
-                        <li>
-                            <a href="#">
-                                <img src="assets/images/footer/linked.svg" loading="lazy" alt=""/>
-
-                                <span>
-                      لينكدين
-                    </span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                <img src="assets/images/footer/insta.svg" loading="lazy" alt=""/>
-
-                                <span>
-                      انستغرام
-                    </span>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                <img src="assets/images/footer/facebook.svg" loading="lazy" alt=""/>
-
-                                <span>
-                      فيسبوك
-                    </span>
-                            </a>
-                        </li>
-                    </ul>
+                    <p>
+                        هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى،
+                        حيث
+                        يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
+                        التطبيق.هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                        العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف
+                        التى
+                        يولدها التطبيق.
+                    </p>
                 </div>
+
             </div>
-        </div>
-
-        <div class="copyrights">
-            <p>
-                © 2024 جوابا. جميع الحقوق محفوظة.
-            </p>
-
-            <ul class="links">
-                <li>
-                    <a href="#">
-                        إعدادات ملفات تعريف الارتباط
-                    </a>
-                </li>
-
-                <li>
-                    <a href="use-terms.html">
-                        شروط الخدمة
-                    </a>
-                </li>
-
-                <li>
-                    <a href="privacy-policy.html">
-                        سياسة الخصوصية
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</footer>
-
-<script src="assets/js/lib/jquery4.js"></script>
-<script src="assets/js/lib/popper.js"></script>
-<script src="assets/js/lib/bootstrap.js"></script>
-<script src="assets/js/lib/swiper-bundle.min.js"></script>
-<script src="assets/js/main.js"></script>
-</body>
-</html>
+        </section>
+    </main>
+@endsection
