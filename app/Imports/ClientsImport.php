9b9c4e9e54cfe1b613b3d7547cb07f71f9add6c9
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-08-08 00:47:23 +0300

    add GuapaPlus to Dashbourd

---
+++ app/Imports/ClientsImport.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Imports;
+
+use App\Helpers\Common;
+use App\Models\Setting;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Validators\Failure;
+
+class ClientsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnFailure
+{
+    use Importable;
+
+
+    private $failures = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function model(array $row)
+    {
+        return [
+            'name' => $row['name'],
+            'phone' => $row['phone'],
+        ];
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' =>  'required|string|min:3|max:100',
+            'phone' => 'required|string|' . (Setting::isAllMobileNumsAccepted() ? '' : Common::phoneValidation()),
+        ];
+    }
+
+    public function onFailure(Failure ...$failures)
+    {
+        $this->failures = array_merge($this->failures, $failures);
+    }
+
+    public function getFailures()
+    {
+        return $this->failures;
+    }
+}
