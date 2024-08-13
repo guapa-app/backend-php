5d798bb5970f3380a3ac3f747da7d68bffc47fe9
ADdora <abdelkaderx3@gmail.com>
2024-08-13 15:05:33 +0300

    Handle product (offer) list for vendors and users with relations - categories - vendor data

--- app/Repositories/Eloquent/OfferRepository.php
+++ app/Repositories/Eloquent/OfferRepository.php
@@ -21,7 +21,7 @@ class OfferRepository extends EloquentRepository implements OfferRepositoryInter
     /**
      * Construct an instance of the repo.
      *
-     * @param \App\Models\Offer $model
+     * @param Offer $model
      */
     public function __construct(Offer $model)
     {
@@ -30,20 +30,35 @@ class OfferRepository extends EloquentRepository implements OfferRepositoryInter
 
     public function all(Request $request): object
     {
-        if (empty($request->vendor_id))
-            $request->merge([
-                'vendor_id' => $this->user->managerVendorId(),
-            ]);
-
         $query = Product::query()
             ->applyFilters($request)
             ->applyOrderBy($request->get('sort'), $request->get('order'))
             ->withListRelations($request)
             ->withListCounts($request)
-            ->withApiListRelationsForVendor($request);
+            ->when(!$this->isAdmin(), function ($query) use ($request) {
+                $query->withApiListRelations($request);
+            });
+
+        if ('enduser' === strtolower($request->header('X-App-Type'))) {
+            $query->whereHas('offer');
+        } else {
+            if (empty($request->vendor_id)) {
+                $request->merge([
+                    'vendor_id' => $this->user->managerVendorId(),
+                ]);
+            }
+
+            $query->withAllVendorOffers($request);
+        }
 
         if ($request->has('perPage')) {
-            return $query->paginate($request->perPage);
+            $perPage = (int) ($request->has('perPage') ? $request->get('perPage') : $this->perPage);
+
+            if ($perPage > 50) {
+                $perPage = 50;
+            }
+
+            return $query->paginate($perPage);
         } else {
             return $query->get();
         }
