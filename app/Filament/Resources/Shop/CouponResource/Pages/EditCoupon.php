ac326463fba7feaa9833af01899fdfb5987b226e
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-08-16 11:22:28 +0300

    Add Coupon Feature To Provider Dashbourd

---
+++ app/Filament/Resources/Shop/CouponResource/Pages/EditCoupon.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Shop\CouponResource\Pages;
+
+use App\Filament\Resources\Shop\CouponResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCoupon extends EditRecord
+{
+    protected static string $resource = CouponResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
