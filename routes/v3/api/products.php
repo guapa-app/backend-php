3ae815d9e27804e92bdd83653eaa99c66edf4633
ADdora <abdelkaderx3@gmail.com>
2024-08-18 23:44:13 +0300

    Handle V3 for Product - add Contoller - add Resource - add Collection - modify api route file - add products route file

---
+++ routes/v3/api/products.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\V3\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',                                              [ProductController::class, 'index']);
+Route::get('/{id}',                                          [ProductController::class, 'single']);
+
+Route::group(['middleware' => 'auth:api'], function() {
+	Route::post('/',                                         [ProductController::class, 'create']);
+	Route::match(['put', 'patch', 'post'], '/{id}', [ProductController::class, 'update']);
+	Route::delete('/{id}',                                   [ProductController::class, 'delete']);
+});
