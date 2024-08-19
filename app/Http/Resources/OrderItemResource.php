20c44f01cdeb5391010673ef9eaf77340b1dbe93
ADdora <abdelkaderx3@gmail.com>
2024-08-19 20:51:43 +0300

    Add payment details to order item, that help to prevent any issue. - add product title. - add current order taxes. - add current paid amount for item, depending on fees or fixed price. - update order item resource. - handle invoice to display data depending on changes.

--- app/Http/Resources/OrderItemResource.php
+++ app/Http/Resources/OrderItemResource.php
@@ -12,12 +12,14 @@ class OrderItemResource extends JsonResource
             'id'           => $this->id,
             'coupon_num'   => (string) $this->coupon_num,
             'amount'       => (float) $this->amount,
+            'title'        => (float) $this->title,
+            'amount_to_pay'=> (float) $this->amount_to_pay,
+            'taxes'        => (float) $this->taxes,
             'quantity'     => (int) $this->quantity,
             'order_id'     => $this->order_id,
             'appointment'  => $this->appointment,
 
             'product'      => ProductResource::make($this->whenLoaded('product')),
-//            "offer"        => OfferResource::make($this->whenLoaded('offer')),
             'vendor'       => VendorResource::make($this->whenLoaded('vendor')),
         ];
     }
