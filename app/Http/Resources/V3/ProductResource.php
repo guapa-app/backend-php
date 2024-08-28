95a47c2979db09896c9010b93431f94a11a62a6f
ADdora <abdelkaderx3@gmail.com>
2024-08-28 08:46:34 +0300

    update v3 product resource

--- app/Http/Resources/V3/ProductResource.php
+++ app/Http/Resources/V3/ProductResource.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Resources\V3;
 
+use App\Http\Resources\AddressResource;
 use App\Http\Resources\MediaResource;
 use App\Http\Resources\OfferResource;
+use App\Http\Resources\VendorResource;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\MissingValue;
 
 class ProductResource extends JsonResource
 {
@@ -14,24 +15,21 @@ class ProductResource extends JsonResource
         $returned_arr = [
             'id'                                    => $this->id,
             'hash_id'                               => (string) $this->hash_id,
-//            'vendor_id'                             => $this->vendor_id,
             'title'                                 => (string) $this->title,
             'description'                           => (string) $this->description,
             'address'                               => (string) $this->address,
             'taxonomy_name'                         => (string) $this->taxonomy_name,
             'price'                                 => (float) $this->price,
             'status'                                => $this->status,
-//            'review'                                => $this->review,
-//            'type'                                  => $this->type,
-//            'terms'                                 => (string) $this->terms,
-//            'url'                                   => (string) $this->url,
-//            'likes_count'                           => (int) $this->likes_count,
-//            'is_liked'                              => (bool) $this->is_liked,
-            'payment_details'                        => $this->payment_details,
+            'type'                                  => $this->type,
+            'terms'                                 => (string) $this->terms,
+            'is_liked'                              => (bool) $this->is_liked,
+            'shared_link'                           => $this->shared_link,
+            'payment_details'                       => $this->payment_details,
+
             'offer'                                 => OfferResource::make($this->whenLoaded('offer')),
-//            'vendor'                                => VendorResource::make($this->whenLoaded('vendor')),
-//            'taxonomies'                            => TaxonomyResource::collection($this->whenLoaded('taxonomies')),
-//            'addresses'                             => AddressResource::collection($this->whenLoaded('addresses')),
+            'vendor'                                => VendorResource::make($this->whenLoaded('vendor')),
+            'addresses'                             => AddressResource::collection($this->whenLoaded('addresses')),
             'images'                                => MediaResource::collection($this->whenLoaded('media')),
         ];
 
