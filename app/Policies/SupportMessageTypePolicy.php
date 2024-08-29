9cb8021b5770ff4aac4af6eb498e5265c3ea769e
ADdora <abdelkaderx3@gmail.com>
2024-08-29 20:24:57 +0300

    Handle Markter role from admin panel.     add policies for Coupon     add policies for Influencer     add policies for ShareLink     add policies for SocialMedia     add policies for SupportMessageType     add seeder for Marketer     update other roles with missing permissions

---
+++ app/Policies/SupportMessageTypePolicy.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\SupportMessageType;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Database\Eloquent\Model;
+
+class SupportMessageTypePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(Model $user)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('view_support_message_type// s');
+            } else {
+                return true;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(Model $user, SupportMessageType $support_message_type)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('view_support_message_types');
+            } else {
+                return true;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(Model $user)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('create_support_message_types');
+            } else {
+                return true;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(Model $user, SupportMessageType $support_message_type)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('update_support_message_types');
+            } else {
+                return true;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(Model $user, SupportMessageType $support_message_type)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('delete_support_message_types');
+            } else {
+                return false;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(Model $user, SupportMessageType $support_message_type)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('update_support_message_types');
+            } else {
+                return false;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(Model $user, SupportMessageType $support_message_type)
+    {
+        try {
+            if ($user->isAdmin()) {
+                return $user->hasPermissionTo('delete_support_message_types');
+            } else {
+                return false;
+            }
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+}
---
+++ database/seeders/MarketerSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class MarketerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::beginTransaction();
+        try {
+            $modules = [
+                'Influencer', 'Coupon', 'ShareLink', 'SocialMedia', 'SupportMessageType',
+            ];
+            $permissions_arr = ['view_', 'create_', 'update_', 'delete_'];
+            $permissions = [];
+
+            foreach ($modules as $module) {
+                foreach ($permissions_arr as $permission) {
+                    $permissions[] = Permission::create([
+                        'name' => $permission . Str::snake(Str::plural($module)),
+                        'guard_name' => 'admin', // Ensure the guard_name is 'admin'
+                    ]);
+                }
+            }
+
+            $marketer = Role::create(['guard_name' => 'admin', 'name' => 'marketer']);
+
+            $marketer->givePermissionTo(['view_influencers', 'create_influencers', 'update_influencers', 'delete_influencers']);
+
+            $roles = Role::query()
+                ->where('guard_name', 'admin')
+                ->whereNot('name', 'marketer')
+                ->get(); // Fetch roles with 'admin' guard
+
+            foreach ($roles as $role) {
+                $role->givePermissionTo($permissions);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack(); // Rollback the transaction in case of error
+            throw $e;
+        }
+    }
+}
