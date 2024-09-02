7656da333effdc8078782c24661cb0f349aa555b
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-02 18:54:51 +0300

    Generate Qrcode Link
    - Create new QrcodeService to generate qrcode as image
    - Update Create order to include the new method
    - Add the new attribute to OrderItemResource.php
    - Add new column called qr_code_link to order_items table

---
+++ app/Services/QrCodeService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class QrCodeService
+{
+    public function generate(array $data): string
+    {
+        $qrCodeString = "hash-id: ".$data['hash_id'];
+        $qrCodeString .= " title: ".$data['title'];
+        $qrCodeString .= " remain_amount: ".$data['amount_to_pay'];
+        $qrCodeString .= " vendor_name: ".$data['vendor_name'];
+
+        $file = QrCode::size(200)->generate(utf8_encode($qrCodeString));
+
+        $path = '/public/qrcode/'.$data['hash_id'].'_'.rand(1000, 9999).'.png';
+
+        Storage::disk('s3')->put($path, $file, 'public');
+
+        return $this->getGeneratedQrCode($path);
+    }
+
+    public function getGeneratedQrCode(string $path): string
+    {
+        return Storage::disk('s3')->url($path);
+    }
+}
