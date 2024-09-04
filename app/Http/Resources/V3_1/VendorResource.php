17b7a0133a0cac8ab76dd39646f1ccf6197e71d4
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-04 08:57:55 +0300

    Refactor Enduser Vendor&Product Screen
    - Update ProductController
    - Update ProductResource
    - Update VendorController
    - Update VendorResource

--- app/Http/Resources/V3_1/VendorResource.php
+++ app/Http/Resources/V3_1/VendorResource.php
@@ -12,8 +12,21 @@ class VendorResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => (string) $this->name,
+            'email' => (string) $this->email,
+            'phone' => (string) $this->phone,
+            'about' => (string) $this->about,
+            'verified' => (bool) $this->verified,
+            'whatsapp' => (string) $this->whatsapp,
+            'twitter' => (string) $this->twitter,
+            'instagram' => (string) $this->instagram,
+            'working_days' => (string) $this->working_days,
+            'working_hours' => (string) $this->working_hours,
+            'snapchat' => (string) $this->snapchat,
+            'website_url' => (string) $this->website_url,
             'addresses' => AddressResource::collection($this->whenLoaded('addresses')),
             'logo' => MediaResource::make($this->whenLoaded('logo')),
+            'products' => ProductResource::collection($this->whenLoaded('products')),
+            'services' => ProductResource::collection($this->whenLoaded('services')),
         ];
     }
 }
