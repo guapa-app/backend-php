d27821908391d828f6ae39e920a8ff5ba3b5367e
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-09-04 18:22:42 +0300

    chenge collection path

--- app/Http/Controllers/Api/User/V3_1/FavoriteController.php
+++ app/Http/Controllers/Api/User/V3_1/FavoriteController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\User\V3_1;
 
 use App\Http\Controllers\Api\FavoriteController as ApiFavoriteController;
 use App\Http\Requests\FavoriteRequest;
-use App\Http\Resources\V3_1\FavoriteCollection;
-use App\Http\Resources\V3_1\FavoriteResource;
+use App\Http\Resources\V3_1\User\FavoriteCollection;
+use App\Http\Resources\V3_1\User\FavoriteResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
