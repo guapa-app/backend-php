b431e240dd7607d5426753d1a5523bacf53a16c7
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-04 09:17:43 +0300

    Refactor Enduser Vendor Screen
    - Update VendorController.php
    - Update vendors.php

--- app/Http/Controllers/Api/User/V3_1/VendorController.php
+++ app/Http/Controllers/Api/User/V3_1/VendorController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers\Api\User\V3_1;
 
 use App\Http\Controllers\Api\VendorController as ApiVendorController;
-use App\Http\Requests\VendorRequest;
 use App\Http\Resources\V3_1\VendorResource;
-use Exception;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class VendorController extends ApiVendorController
@@ -31,47 +28,5 @@ class VendorController extends ApiVendorController
                 'success' => true,
                 'message' => __('api.success'),
             ]);
-    }
-
-    /**
-     * After register a user he can have a vendor access
-     * by add vendor data.
-     *
-     * @param  VendorRequest  $request
-     * @return VendorResource|JsonResponse
-     */
-    public function create(VendorRequest $request)
-    {
-        try {
-            $vendor = parent::create($request);
-
-            return VendorResource::make($vendor)
-                ->additional([
-                    'success' => true,
-                    'message' => __('api.created'),
-                ]);
-        } catch (Exception $exception) {
-            $this->logReq($exception->getMessage());
-
-            return $this->errorJsonRes(message: 'something went wrong');
-        }
-    }
-
-    public function update(VendorRequest $request, $id)
-    {
-        $item = parent::update($request, $id);
-
-        return VendorResource::make($item)
-            ->additional([
-                'success' => true,
-                'message' => __('api.updated'),
-            ]);
-    }
-
-    public function share(Request $request, $id)
-    {
-        $sharesCount = parent::share($request, $id);
-
-        return $this->successJsonRes($sharesCount, __('api.success'));
     }
 }
--- routes/user/v3_1/api/vendors.php
+++ routes/user/v3_1/api/vendors.php
@@ -1,14 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\User\V3_1\VendorClientController;
 use App\Http\Controllers\Api\User\V3_1\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api', 'as' => 'vendors.'], function () {
-    Route::post('/', [VendorController::class, 'create'])->name('create');
-    Route::match(['put', 'patch', 'post'], '/{id}', [VendorController::class, 'update'])->name('update');
-
-    Route::apiResource('{vendor}/clients', VendorClientController::class)->only(['index', 'store', 'destroy']);
-    Route::get('/{vendor}/clients/{client}/orders',
-        [VendorClientController::class, 'getClientOrders'])->name('client.orders');
+    Route::get('/', [VendorController::class, 'index'])->name('list');
+    Route::get('/{id}', [VendorController::class, 'single'])->name('vendors.show');
 });
