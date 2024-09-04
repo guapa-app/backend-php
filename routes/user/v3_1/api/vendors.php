77bbb6168a4035eddfe91fbb17713c2bbb2f66f2
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-04 09:18:51 +0300

    Refactor Enduser Vendor Screen
    - Update VendorController.php
    - Update vendors.php

--- routes/user/v3_1/api/vendors.php
+++ routes/user/v3_1/api/vendors.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\Api\User\V3_1\VendorController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'auth:api', 'as' => 'vendors.'], function () {
-    Route::get('/', [VendorController::class, 'index'])->name('list');
-    Route::get('/{id}', [VendorController::class, 'single'])->name('vendors.show');
-});
+Route::get('/', [VendorController::class, 'index'])->name('list');
+Route::get('/{id}', [VendorController::class, 'single'])->name('vendors.show');
