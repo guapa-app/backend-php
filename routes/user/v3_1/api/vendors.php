0e902b7e2e02abb12ba14b8c5f07ad1ca2e850d5
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-04 09:13:56 +0300

    Refactor Enduser Vendor Screen
    - Remove InfluencerController from user
    - Update vendors.php

--- routes/user/v3_1/api/vendors.php
+++ routes/user/v3_1/api/vendors.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\User\V3_1\InfluencerController;
 use App\Http\Controllers\Api\User\V3_1\VendorClientController;
 use App\Http\Controllers\Api\User\V3_1\VendorController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\User\V3_1\VendorSocialMediaController;
 
 Route::group(['middleware' => 'auth:api', 'as' => 'vendors.'], function () {
     Route::post('/', [VendorController::class, 'create'])->name('create');
@@ -13,11 +11,4 @@ Route::group(['middleware' => 'auth:api', 'as' => 'vendors.'], function () {
     Route::apiResource('{vendor}/clients', VendorClientController::class)->only(['index', 'store', 'destroy']);
     Route::get('/{vendor}/clients/{client}/orders',
         [VendorClientController::class, 'getClientOrders'])->name('client.orders');
-
-    Route::apiResource('{vendor}/social-media', VendorSocialMediaController::class)->only([
-        'store', 'update', 'destroy'
-    ]);
-    Route::apiResource('{vendor}/influencers', InfluencerController::class)->only([
-        'index', 'store', 'update', 'destroy'
-    ]);
 });
