cffca05633519902a89855df4d5855a999345df2
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-09-19 00:25:19 +0300

    fix migration issue

--- database/migrations/2024_09_17_171904_add_polymoric_columns_to_invoices_table.php
+++ database/migrations/2024_09_17_171904_add_polymoric_columns_to_invoices_table.php
@@ -10,41 +10,53 @@ return new class extends Migration
     {
         // Add new columns
         Schema::table('invoices', function (Blueprint $table) {
-            $table->after('order_id', function ($table) {
-                $table->morphs('invoiceable');
-            });
+            if (!Schema::hasColumn('invoices', 'invoiceable_type') && !Schema::hasColumn('invoices', 'invoiceable_id')) {
 
+                $table->after('order_id', function ($table) {
+                    $table->morphs('invoiceable');
+                });
+            }
         });
 
-        // Migrate existing data
-        DB::table('invoices')->whereNotNull('order_id')->update([
-            'invoiceable_type' => 'App\\Models\\Order',
-            'invoiceable_id' => DB::raw('order_id')
-        ]);
-
-        DB::table('invoices')->whereNotNull('marketing_campaign_id')->update([
-            'invoiceable_type' => 'App\\Models\\MarketingCampaign',
-            'invoiceable_id' => DB::raw('marketing_campaign_id')
-        ]);
-
-        // Drop old columns
-//        Schema::table('invoices', function (Blueprint $table) {
-//            $table->dropForeign(['order_id']);
-//            $table->dropColumn('order_id');
-//            $table->dropColumn('marketing_campaign_id');
-//        });
+        Schema::table('invoices', function (Blueprint $table) {
+            if (Schema::hasColumn('invoices', 'order_id')) {
+                // Migrate existing data
+                DB::table('invoices')->whereNotNull('order_id')->update([
+                    'invoiceable_type' => 'App\\Models\\Order',
+                    'invoiceable_id' => DB::raw('order_id')
+                ]);
+                // Drop old column
+                $table->dropColumn('order_id');
+            }
+            if (Schema::hasColumn('invoices', 'marketing_campaign_id')) {
+                // Migrate existing data
+                DB::table('invoices')->whereNotNull('marketing_campaign_id')->update([
+                    'invoiceable_type' => 'App\\Models\\MarketingCampaign',
+                    'invoiceable_id' => DB::raw('marketing_campaign_id')
+                ]);
+                // Drop old column
+                $table->dropColumn('marketing_campaign_id');
+            }
+        });
     }
-
     public function down()
     {
+        // Add old columns
         Schema::table('invoices', function (Blueprint $table) {
-            $table->unsignedBigInteger('order_id')->nullable();
-            $table->unsignedBigInteger('marketing_campaign_id')->nullable();
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('order_id')->nullable()->after('id');
+                $table->unsignedBigInteger('marketing_campaign_id')->nullable()->after('order_id');
 
-            $table->dropIndex(['invoiceable_type', 'invoiceable_id']);
-            $table->dropColumn('invoiceable_type');
-            $table->dropColumn('invoiceable_id');
+            $table->foreign('order_id')
+                ->references('id')
+                ->on('orders')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreign('marketing_campaign_id')
+                ->references('id')
+                ->on('marketing_campaigns')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
         });
 
         // Migrate data back (this will lose any new types of invoiceables added)
@@ -55,5 +67,12 @@ return new class extends Migration
         DB::table('invoices')
             ->where('invoiceable_type', 'App\\Models\\MarketingCampaign')
             ->update(['marketing_campaign_id' => DB::raw('invoiceable_id')]);
+
+        // Drop new columns
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn('invoiceable_type');
+            $table->dropColumn('invoiceable_id');
+        });
+
     }
 };
