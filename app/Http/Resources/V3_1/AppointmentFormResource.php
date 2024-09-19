014e6b67a00a64de29d446d6903f3b8386ae3ae6
ADdora <abdelkaderx3@gmail.com>
2024-09-19 21:17:43 +0300

    HotFix - add v3.1 resource as temporary to remove later

---
+++ app/Http/Resources/V3_1/AppointmentFormResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\V3_1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AppointmentFormResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'key' => $this->key,
+            'type' => $this->type,
+            'values' => AppointmentFormValuesResource::collection($this->whenLoaded('values')),
+        ];
+    }
+}
---
+++ app/Http/Resources/V3_1/User/FavoriteCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Resources\V3_1\User;
+
+use App\Http\Resources\V3_1\PostCollection;
+use App\Http\Resources\V3_1\ProductCollection;
+use App\Http\Resources\V3_1\VendorCollection;
+use App\Models\Offer;
+use App\Models\Post;
+use App\Models\Product;
+use App\Models\Vendor;
+
+class FavoriteCollection extends GeneralCollection
+{
+}
