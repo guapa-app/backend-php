dba3ddcdc9990a8feead78ec7303b2890413fa23
Abdelrahman-Labib <abdolabib601@gmail.com>
2024-09-25 18:49:30 +0300

    Update User & Vendor routes
    - Add MarketingCampaignController to v3-1
    - Add socialMedia to v3-1

--- routes/vendor/v3_1/api.php
+++ routes/vendor/v3_1/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Api\Vendor\V3_1\DeviceController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\Vendor\V3_1\DataController;
 use App\Http\Controllers\Api\Vendor\V3_1\HomeController;
-use App\Http\Controllers\Api\Vendor\V3_1\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("vendor/v3.1")->middleware('api-vendor')->group(function () {
@@ -32,6 +31,7 @@ Route::prefix("vendor/v3.1")->middleware('api-vendor')->group(function () {
     Route::prefix('campaigns')->group(base_path('routes/vendor/v3_1/api/campaigns.php'));
     Route::prefix('support-msg')->group(base_path('routes/vendor/v3_1/api/support.php'));
     Route::prefix('appointments')->group(base_path('routes/vendor/v3_1/api/appointments.php'));
+    Route::prefix('campaigns')->group(base_path('routes/vendor/v3_1/api/campaigns.php'));
 
     Route::post('devices', [DeviceController::class, 'addDevice'])->middleware('auth:api');
     Route::get('data', [DataController::class, 'data']);
