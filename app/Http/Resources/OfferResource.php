eb6a53b22b994680928c81cefa6481c5e2795665
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-09-29 17:53:16 +0300

    update offer price format &  responses on v3.1

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -14,7 +14,7 @@ class OfferResource extends JsonResource
             'title'                 => $this->title,
             'description'           => $this->description,
             'terms'                 => $this->terms,
-            'price'                 => (float) $this->price,
+            'price'                 => number_format((float) $this->price, 1, '.', ''), // Ensuring one decimal place
             'discount'              => $this->discount,
             'discount_string'       => $this->discount_string,
             'status'                => $this->status,
--- app/Http/Resources/User/V3_1/OfferResource.php
+++ app/Http/Resources/User/V3_1/OfferResource.php
@@ -9,13 +9,18 @@ class OfferResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'discount_string' => $this->discount_string,
-            'discount' => $this->discount,
-            'starts_at' => $this->starts_at,
-            'expires_at' => $this->expires_at,
-            'image' => MediaResource::make($this->whenLoaded('image')),
+            'id'                  => $this->id,
+            'title'               => $this->title,
+            'description'         => $this->description,
+            'terms'               => $this->terms,
+            'discount_string'     => $this->discount_string,
+            'discount'            => $this->discount,
+            'starts_at'           => $this->starts_at->format('Y-m-d H:i:s'),
+            'expires_at'          => $this->expires_at->format('Y-m-d H:i:s'),
+            'price'               => number_format((float) $this->price, 1, '.', ''), // Ensuring one decimal place
+            'status'              => $this->status,
+            'expires_countdown'   => $this->expires_countdown,
+            'image'               => MediaResource::make($this->whenLoaded('image')),
         ];
     }
 }
