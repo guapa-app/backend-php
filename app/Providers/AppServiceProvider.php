6cf2c7c2e07f96d64db3961c10244cf7d67d1202
ADdora <abdelkaderx3@gmail.com>
2024-10-03 19:00:43 +0300

    move bind whatsapp service to channel provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Contracts\WhatsAppServiceInterface;
-use App\Services\ConnectlyWhatsAppService;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
@@ -18,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(WhatsAppServiceInterface::class, ConnectlyWhatsAppService::class);
+        //
     }
 
     /**
--- app/Providers/ChannelServiceProvider.php
+++ app/Providers/ChannelServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Channels\FirebaseChannel;
 use App\Channels\WhatsAppChannel;
 use App\Contracts\WhatsAppServiceInterface;
+use App\Services\ConnectlyWhatsAppService;
 use App\Services\FirebaseService;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +17,8 @@ class ChannelServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(WhatsAppServiceInterface::class, ConnectlyWhatsAppService::class);
+
         // Register the FirebaseChannel with dependency injection for FirebaseService
         $this->app->singleton(FirebaseChannel::class, function ($app) {
             return new FirebaseChannel($app->make(FirebaseService::class));
