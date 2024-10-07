ad74c2969af35479a8a75d59c2fe0bef9eca5ac7
kareemesaam <42080439+kareemesaam@users.noreply.github.com>
2024-10-07 21:50:09 +0300

    merge staging and check payment status

--- app/Http/Controllers/Api/User/V3_1/PaymentController.php
+++ app/Http/Controllers/Api/User/V3_1/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\User\V3_1;
 use App\Http\Controllers\Api\BaseApiController;
 use App\Http\Requests\V3_1\Common\PaymentStatusRequest;
 use App\Services\MarketingCampaignService;
+use App\Services\PaymentService;
 use App\Services\V3_1\AppointmentOfferService;
 use App\Services\V3_1\OrderService;
 use Illuminate\Support\Facades\Log;
@@ -14,12 +15,20 @@ class PaymentController extends BaseApiController
     protected $orderService;
     protected $marketingCampaignService;
     protected $appointmentOfferService;
-    public function __construct(OrderService $orderService, MarketingCampaignService $marketingCampaignService, AppointmentOfferService $appointmentOfferService)
+    protected $paymentService;
+
+    public function __construct(
+        OrderService $orderService,
+        MarketingCampaignService $marketingCampaignService,
+        AppointmentOfferService $appointmentOfferService,
+        PaymentService $paymentService
+    )
     {
         parent::__construct();
         $this->orderService = $orderService;
         $this->marketingCampaignService = $marketingCampaignService;
         $this->appointmentOfferService = $appointmentOfferService;
+        $this->paymentService = $paymentService;
     }
 
     /**
@@ -34,26 +43,31 @@ class PaymentController extends BaseApiController
     public function changePaymentStatus(PaymentStatusRequest $request)
     {
         $data = $request->validated();
-        $type = $request->type;
-        try {
-            switch ($type) {
-                case 'order':
-                    $this->orderService->changeOrderStatus($data);
-                    break;
-                case 'campaign':
-                    $this->marketingCampaignService->changePaymentStatus($data);
-                    break;
-                case 'appointment':
-                    $this->appointmentOfferService->changePaymentStatus($data);
-                    break;
-                default:
-                    return $this->errorJsonRes([], __('api.invalid_type'));
-            }
+        $payment_id = $request->payment_id;
+        if ($this->paymentService->isPaymentPaidSuccessfully($payment_id)) {
+            $type = $request->type;
+            try {
+                switch ($type) {
+                    case 'order':
+                        $this->orderService->changeOrderStatus($data);
+                        break;
+                    case 'campaign':
+                        $this->marketingCampaignService->changePaymentStatus($data);
+                        break;
+                    case 'appointment':
+                        $this->appointmentOfferService->changePaymentStatus($data);
+                        break;
+                    default:
+                        return $this->errorJsonRes([], __('api.invalid_type'));
+                }
 
-            return $this->successJsonRes([], __('api.payment_status_changed'));
-        } catch (\Exception $e) {
-            Log::error('Error changing payment status: ' . $e->getMessage());
-            return $this->errorJsonRes([], __('api.error_payment_status'));
+                return $this->successJsonRes([], __('api.payment_status_changed'));
+            } catch (\Exception $e) {
+                Log::error('Error changing payment status: ' . $e->getMessage());
+                return $this->errorJsonRes([], __('api.error_payment_status'));
+            }
+        } else {
+            return $this->errorJsonRes([], __('Payment details are incorrect. Please check your payment again.'));
         }
     }
 }
