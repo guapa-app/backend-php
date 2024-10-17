9cf2375fe16f4e0979872ea84f4cdcf9fc7b562b
ADdora <abdelkaderx3@gmail.com>
2024-10-17 16:35:53 +0300

    Handle dashboard widgets  order chart  customer chart  latest orders  overview states

---
+++ app/Filament/Admin/Widgets/StatsOverviewWidget.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Models\Order;
+use App\Models\Post;
+use App\Models\Product;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class StatsOverviewWidget extends BaseWidget
+{
+    protected static ?string $pollingInterval = null;
+
+    protected function getCards(): array
+    {
+        $products = Trend::query(Product::query())
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        $posts = Trend::query(Post::query())
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        $orders = Trend::query(Order::query())
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            Card::make(__('Products'), $products->sum(fn(TrendValue $value) => $value->aggregate))
+                ->description(number_format($products->average(fn(TrendValue $value) => $value->aggregate), 1) . ' ' . __('per month', ['type' => __('product')]))
+                ->descriptionIcon('heroicon-s-clock')
+                ->chart($products->map(fn(TrendValue $value) => $value->aggregate)->toArray())
+                ->color('success'),
+
+            Card::make(__('Posts'), $posts->sum(fn(TrendValue $value) => $value->aggregate))
+                ->description(number_format($posts->average(fn(TrendValue $value) => $value->aggregate), 1) . ' ' . __('per month', ['type' => __('post')]))
+                ->descriptionIcon('heroicon-s-clock')
+                ->chart($posts->map(fn(TrendValue $value) => $value->aggregate)->toArray())
+                ->color('danger'),
+
+            Card::make(__('Orders'), $orders->sum(fn(TrendValue $value) => $value->aggregate))
+                ->description(number_format($orders->average(fn(TrendValue $value) => $value->aggregate), 1) . ' ' . __('per month', ['type' => __('order')]))
+                ->descriptionIcon('heroicon-s-clock')
+                ->chart($orders->map(fn(TrendValue $value) => $value->aggregate)->toArray())
+                ->color('warning'),
+        ];
+    }
+}
