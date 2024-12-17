a449683e258fd101196056e3e0aeee1e24afb67e
Feras Alshomer <info@guapa.com.sa>
2024-12-17 13:09:41 +0000

    Update blogs.blade.php Ads Blogs

--- resources/views/frontend/blogs.blade.php
+++ resources/views/frontend/blogs.blade.php
@@ -1,3 +1,14 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11502298872">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11502298872');
+</script>
+
 @extends('frontend.layouts.app')
 
 @php
