1e5a3bebdf9eb62227bfa5353b89fc5e5d13453c
Abdulrahman Muahmmed <abdulrahman.elnegery@gmail.com>
2025-04-26 21:46:34 +0300

    enhance consultations manager realtion

---
+++ app/Filament/Admin/Resources/UserVendor/ConsultationResource/Pages/ViewConsultation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\UserVendor\VendorResource\Pages;
+
+use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Admin\Resources\UserVendor\ConsultationResource;
+
+class ViewConsultation extends ViewRecord
+{
+    protected static string $resource = ConsultationResource::class;
+}
