04206fb644e16888c85a8e294e3465eba5fa2872
Abdulrahman Muahmmed <abdulrahman.elnegery@gmail.com>
2025-06-25 22:44:50 +0300

    fix: resolve route naming conflicts in user v3_1 API routes
    - Add version prefixes to auth routes (v3_1.auth.*)
    - Add version prefixes to cities routes (v3_1.cities.*)
    - Add version prefixes to coupons routes (v3_1.coupons.*)
    - Add version prefixes to pages routes (v3_1.pages.*)
    - Add version prefixes to support routes (v3_1.support.*)
    - Add version prefixes to users routes (v3_1.users.*)
    - Add version prefixes to vendors routes (v3_1.vendors.*)
    - Fix apiResource route names with unique prefixes

--- routes/user/v3_1/api/vendors.php
+++ routes/user/v3_1/api/vendors.php
@@ -1,21 +1,25 @@
 <?php
 
-use App\Http\Controllers\Api\User\V3_1\VendorController;
-use App\Http\Controllers\Api\Vendor\V3_1\VendorSocialMediaController;
-use App\Http\Controllers\Api\User\V3_1\ConsultationController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\User\V3_1\VendorController;
+use App\Http\Controllers\Api\User\V3_1\ConsultationController;
+use App\Http\Controllers\Api\Vendor\V3_1\VendorSocialMediaController;
 
-Route::get('/', [VendorController::class, 'index'])->name('list');
-Route::get('/{id}', [VendorController::class, 'single'])->name('vendors.show');
+Route::get('/', [VendorController::class, 'index'])->name('v3_1.vendors.list');
+Route::get('/{id}', [VendorController::class, 'single'])->name('v3_1.vendors.show');
 
 // get available time slots for a vendor
-Route::get('/{vendor}/available-times/{date}', [ConsultationController::class, 'getAvailableTimeSlots'])->name('vendors.available-time-slots');
+Route::get('/{vendor}/available-times/{date}', [ConsultationController::class, 'getAvailableTimeSlots'])->name('v3_1.vendors.available-time-slots');
 
-Route::group(['middleware' => 'auth:api', 'as' => 'vendors.'], function () {
-    Route::post('/', [VendorController::class, 'create'])->name('create');
-    Route::match(['put', 'patch', 'post'], '/{id}', [VendorController::class, 'update'])->name('update');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('/', [VendorController::class, 'create'])->name('v3_1.vendors.create');
+    Route::match(['put', 'patch', 'post'], '/{id}', [VendorController::class, 'update'])->name('v3_1.vendors.update');
 
     Route::apiResource('{vendor}/social-media', VendorSocialMediaController::class)->only([
         'store', 'update', 'destroy'
+    ])->names([
+        'store' => 'v3_1.vendors.social-media.store',
+        'update' => 'v3_1.vendors.social-media.update',
+        'destroy' => 'v3_1.vendors.social-media.destroy'
     ]);
 });
