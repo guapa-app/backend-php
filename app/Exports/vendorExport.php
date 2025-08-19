4201d4d49e4b80c84576a5c7ad453669ddf00215
Mustafa Shoukri <mustafaemam98@gmail.com>
2025-08-19 23:22:36 +0300

    Feat: make exports for users & vendors

---
+++ app/Exports/vendorExport.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Vendor;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class VendorExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Vendor::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Name',
+            'Email',
+            'Phone',
+            'Status'
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->name,
+            $user->email,
+            $user->phone,
+            $user->status == 1 ? 'Active' : 'Disabled',
+        ];
+    }
+}
